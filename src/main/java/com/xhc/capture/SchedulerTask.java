d67b312274e570a4c9145786a0fa4f75d7c80d3f
huajia2005 <861175502@qq.com>
2019-11-01 11:32:33 +0800

    代码重构,时间改为LocalDateTime

--- src/main/java/com/xhc/capture/SchedulerTask.java
+++ src/main/java/com/xhc/capture/SchedulerTask.java
@@ -1,5 +1,6 @@
 package com.xhc.capture;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @version 1.0
@@ -19,33 +20,75 @@ import java.util.Date;
  */
 @Component
 public class SchedulerTask {
-   // private ;//"yyyy-MM-dd:HH:mm");
-    private  String path="C:\\Users\\Administrator\\Desktop";
+    @Value("${image.path}")
+    private String path;
 
     /**
-     * 定时对桌面进行截图.
-     * @throws IOException
-     * @throws AWTException
+     * 定时对桌面进行截图并保存
+     *
+     * @Author xuhongchun
+     * @Description
+     * @Date 10:47 2019/11/1
+     * @Param []
+     * @return void
      */
-    @Scheduled(fixedDelay = 900000)
+    @Scheduled(fixedDelay = 60000)
     public void capture() throws IOException, AWTException {
-        System.out.println(1);
+        LocalDateTime today = LocalDateTime.now();
+        //对桌面截图
+        BufferedImage image = screenshot();
+        //创建图片文件夹路径
+        File imageDir = createDirectory(today);
+        //创建图片名称
+        String fileName = today.format(DateTimeFormatter.ofPattern("HH-mm-ss")) + ".png";
+        //写入截图图片
+        ImageIO.write(image, "png", new File(imageDir, fileName));
+        System.out.println(today.toString() + "---截图成功");
+    }
+
+    /**
+     * 对桌面进行截图
+     *
+     * @return java.awt.image.BufferedImage
+     * @Author xuhongchun
+     * @Description
+     * @Date 10:04 2019/11/1
+     * @Param []
+     */
+    private BufferedImage screenshot() throws AWTException {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Rectangle screenRectangle = new Rectangle(screenSize);
         Robot robot = new Robot();
-        BufferedImage image = robot.createScreenCapture(screenRectangle);
-        //保存路径
-        SimpleDateFormat sim=new SimpleDateFormat("yyyy-MM-dd");
-        String directory=sim.format(new Date());
-        File screenFile = new File(path+File.separator+directory);
-        if (!screenFile.exists()) {
-            screenFile.mkdir();
+        return robot.createScreenCapture(screenRectangle);
+    }
+
+    /**
+     * 创建文件夹存放路径
+     *
+     * @Author xuhongchun
+     * @Description
+     * @Date 10:56 2019/11/1
+     * @Param [today]
+     * @return java.io.File
+     * @throws
+     */
+    private File createDirectory(LocalDateTime today) {
+        StringBuilder stringBuilder = new StringBuilder(path);
+        String imageDirPath = stringBuilder
+                .append(File.separator)
+                .append(today.getYear())
+                .append(File.separator)
+                .append(today.getMonthValue())
+                .append(File.separator)
+                .append(today.getDayOfMonth())
+                .toString();
+        File imageDir = new File(imageDirPath);
+        if (imageDir.exists()) {
+            return imageDir;
         }
-        sim=new SimpleDateFormat("HH-mm-ss");
-        String files=sim.format(new Date())+".png";
-        File f = new File(screenFile,files);
-        System.out.println(f.getName());
-        ImageIO.write(image, "png", f);
-        //自动打开
+        if (imageDir.mkdirs()) {
+            return imageDir;
+        }
+        return null;
     }
 }
