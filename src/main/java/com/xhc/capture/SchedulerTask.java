50de8be13604e5a6ce3698b69bb87c906a0453ab
huajia2005 <861175502@qq.com>
2020-03-06 01:00:15 +0800

    添加注释,代码更新

--- src/main/java/com/xhc/capture/SchedulerTask.java
+++ src/main/java/com/xhc/capture/SchedulerTask.java
@@ -12,12 +12,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-/**
- * @version 1.0
- * @ClassName SchedulerTask
- * @Author Administrator
- * @Date 2019/6/3 12:46
- */
 @Component
 public class SchedulerTask {
     @Value("${image.path}")
@@ -26,11 +20,10 @@ public class SchedulerTask {
     /**
      * 定时对桌面进行截图并保存
      *
-     * @Author xuhongchun
-     * @Description
-     * @Date 10:47 2019/11/1
-     * @Param []
-     * @return void
+     * @throws IOException  图片写入错误
+     * @throws AWTException 获取错误
+     * @author xuhongchun
+     * @date 12:25 2019/11/1
      */
     @Scheduled(fixedDelay = 60000)
     public void capture() throws IOException, AWTException {
@@ -49,11 +42,10 @@ public class SchedulerTask {
     /**
      * 对桌面进行截图
      *
-     * @return java.awt.image.BufferedImage
-     * @Author xuhongchun
-     * @Description
-     * @Date 10:04 2019/11/1
-     * @Param []
+     * @return {@link BufferedImage} 返回截图缓存图片流
+     * @throws AWTException 获取错误
+     * @author xuhongchun
+     * @date
      */
     private BufferedImage screenshot() throws AWTException {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -65,23 +57,19 @@ public class SchedulerTask {
     /**
      * 创建文件夹存放路径
      *
-     * @Author xuhongchun
-     * @Description
-     * @Date 10:56 2019/11/1
-     * @Param [today]
-     * @return java.io.File
-     * @throws
+     * @param today 当前时间
+     * @return {@link File} 截图存放文件夹
+     * @author xuhongchun
+     * @date
      */
     private File createDirectory(LocalDateTime today) {
-        StringBuilder stringBuilder = new StringBuilder(path);
-        String imageDirPath = stringBuilder
-                .append(File.separator)
-                .append(today.getYear())
-                .append(File.separator)
-                .append(today.getMonthValue())
-                .append(File.separator)
-                .append(today.getDayOfMonth())
-                .toString();
+        String imageDirPath = path +
+                File.separator +
+                today.getYear() +
+                File.separator +
+                today.getMonthValue() +
+                File.separator +
+                today.getDayOfMonth();
         File imageDir = new File(imageDirPath);
         if (imageDir.exists()) {
             return imageDir;
--- src/test/java/com/xhc/capture/CaptureApplicationTests.java
+++ src/test/java/com/xhc/capture/CaptureApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CaptureApplicationTests {
@@ -13,4 +15,10 @@ public class CaptureApplicationTests {
     public void contextLoads() {
     }
 
+    @Test
+    public void localDateTest() {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getMonthValue());
+    }
 }
